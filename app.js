183e78a64733d2e80d71995fd60a05064be5d99e
Aravindan07 <aravindanravi33@gmail.com>
2020-02-16 20:04:21 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express=require('express');
+const path=require('path');
+const request=require('request');
+const app=express();
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.use(express.static('public'));
+
+app.get('/results',(req,res)=>{
+
+    let query=req.query.search;
+
+
+    request('https://api.themoviedb.org/3/search/movie?api_key=63953bb47123a5e58d2e7ecdeaf882e7&query='+query,(error,response,body)=>{
+        if(error){
+            console.log(error);
+        }
+        let data=JSON.parse(body);
+        res.render('movies',{data:data ,searchQuery:query});
+    });
+});
+
+app.get('',(req,res)=>{
+    res.render('search');
+});
+
+
+app.listen('3000',()=>{
+    console.log('Server is running at port 3000');
+});
