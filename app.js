02af2ef020a37b0c88eca3b6d9d62a38e4c6315a
Aravindan <aravindanravi33@gmail.com>
2020-07-01 12:13:43 +0530

    Added changes for deployment to heroku

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
-const express=require('express');
-const path=require('path');
-const request=require('request');
-const app=express();
+const express = require("express");
+const path = require("path");
+const request = require("request");
+const app = express();
 
-app.set('views',path.join(__dirname,'views'));
-app.set('view engine','ejs');
-app.use(express.static('public'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
-app.get('/results',(req,res)=>{
+app.get("/results", (req, res) => {
+  let query = req.query.search;
 
-    let query=req.query.search;
-
-
-    request('https://api.themoviedb.org/3/search/movie?api_key=63953bb47123a5e58d2e7ecdeaf882e7&query='+query,(error,response,body)=>{
-        if(error){
-            console.log(error);
-        }
-        let data=JSON.parse(body);
-        res.render('movies',{data:data ,searchQuery:query});
-    });
+  request(
+    "https://api.themoviedb.org/3/search/movie?api_key=63953bb47123a5e58d2e7ecdeaf882e7&query=" +
+      query,
+    (error, response, body) => {
+      if (error) {
+        console.log(error);
+      }
+      let data = JSON.parse(body);
+      res.render("movies", { data: data, searchQuery: query });
+    }
+  );
 });
 
-app.get('',(req,res)=>{
-    res.render('search');
+app.get("", (req, res) => {
+  res.render("search");
 });
 
-
-app.listen('8080',()=>{
-    console.log('Server is running at port 3000');
+app.listen("8080", () => {
+  console.log("Server is running at port 8080");
 });
